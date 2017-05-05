65b0e0de62408d430202f37dad2e490e58c6c852
wangyong <yundan_wy@126.com>
2017-05-05 16:38:29 +0800

    '项目初始化'

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # netty-tutorial
+> netty 异步的事件驱动的网络应用框架
+官网 netty.io
---
+++ src/test/java/NettyDemo.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * Created by wangyong on 2017/5/5.
+ */
+public class NettyDemo {
+
+    @Test
+    public void test() {
+        System.out.println("hello,netty!");
+    }
+}
